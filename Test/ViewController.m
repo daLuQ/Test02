34c08c544455b88eba5c8b8c8d8c066e2885d898
LiuZiStrugle <15850581247@163.com>
2018-11-26 11:58:34 +0800

    第四次修改

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"---------- ++++++");
+    NSLog(@"---------- ++++++hhhhhhh");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
